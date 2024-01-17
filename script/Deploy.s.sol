136a62083f704afcf3a6bedb58b70bd807a9acef
kevinsslin <kk123750964@gmail.com>
2024-01-18 00:38:49 +0800

    feat: Add PoolFactory deployment and console log

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,9 +2,14 @@
 pragma solidity 0.8.21;
 
 import { BaseScript } from "./Base.s.sol";
+import { PoolFactory } from "../src/PoolFactory.sol";
+import { console } from "forge-std/console.sol";
 
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 contract Deploy is BaseScript {
     //solhint-disable no-empty-blocks
-    function run() public broadcast { }
+    function run() public broadcast { 
+        PoolFactory factory = new PoolFactory();
+        console.log("PoolFactory deployed at address: %s", address(factory));
+    }
 }
